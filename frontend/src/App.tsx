9ca79b345cb53eb34cdb6fa62e7ab6daaa2a1b5b
unknown <abhiperl1000@gmail.com>
2025-02-24 16:49:00 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import {SignedOut ,SignInButton , SignedIn , UserButton } from "@clerk/clerk-react"
+function App() {
+
+  return (
+    <>
+        <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  )
+}
+
+export default App
